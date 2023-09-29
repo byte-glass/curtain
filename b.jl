91e150acb63cc0d64ad4d301549ca9afeb4f404f
awkward crow <awkward.crow@gmail.com>
2023-09-29 18:10:14 +0100

    introduce struct Actor

--- b.jl
+++ b.jl
@@ -1,9 +1,9 @@
-# b.jl
+# b.jc
 
 
 struct Mailbox
-    q::Vector{Any}
-    ch::Channel{Any}
+    queue::Vector{Any}
+    channel::Channel{Any}
     Mailbox() = new(Vector{Any}(), Channel{Any}(32))
 end
 
@@ -11,8 +11,8 @@ nonnull(x) = !isnothing(x)
 nonempty(x) = !isempty(x)
 
 function rec(b, k)
-    while isready(b.ch)
-        push!(b.q, take!(b.ch))
+    while isready(b.channel)
+        push!(b.queue, take!(b.channel))
     end
     found = false
     m = nothing
@@ -20,39 +20,45 @@ function rec(b, k)
         j = copy(k)
         while nonempty(j) && !found
             kappa = popfirst!(j)
-            r = findfirst(kappa, b.q)
+            r = findfirst(kappa, b.queue)
             if nonnull(r)
                 found = true
-                m = popat!(b.q, r)
+                m = popat!(b.queue, r)
             end
         end
         if !found
-            wait(b.ch)
-            while isready(b.ch)
-                push!(b.q, take!(b.ch))
+            wait(b.channel)
+            while isready(b.channel)
+                push!(b.queue, take!(b.channel))
             end
         end
     end
     return m
 end
 
-function send(b::Mailbox, m::Any)
-    put!(b.ch, m)
+function send(b::Mailbox, message::Any)
+    put!(b.channel, message)
     return
 end
 
+struct Actor
+    mailbox::Mailbox
+    task::Task
+end
+
+function send(a::Actor, message::Any)
+    send(a.mailbox, message)
+end
+        
 function spawn(f::Function)
     b = Mailbox()
     t = @task begin f(b); end
     schedule(t)
-    return (; task = t, mailbox = b)
+    return Actor(b, t)
 end
 
 
 ##
-
-
-msg(s) = m -> m[:msg] == s
 
 
 function echo(b::Mailbox)
@@ -64,9 +70,8 @@ end
 
 echo_server = spawn(echo);
 
-send(echo_server[:mailbox], "echo this!")
-send(echo_server[:mailbox], (; msg = :are, p = "you there"))
-
+send(echo_server.mailbox, "echo this!")
+send(echo_server, (; msg = :are, p = "you there"))
 
 
 ##
@@ -80,11 +85,15 @@ end
 
 sink_server = spawn(sink)
 
-send(sink_server[:mailbox], (; m = "eat my shorts", p = 0))
+send(sink_server, (; m = "eat my shorts", p = 0))
+send(sink_server, (; m = "whaaaaa??", p = -1))
 
-sink_server[:mailbox]
+sink_server.mailbox
+
 
 ##
+
+msg(s) = m -> m[:msg] == s
 
 function forward(b::Mailbox)
     m = rec(b, [msg(:please_forward)])
@@ -92,39 +101,45 @@ function forward(b::Mailbox)
 end
 
 forwarder = spawn(forward)
-
 echo_server = spawn(echo)
-sink_server = spawn(sink)
+send(forwarder, (; msg = :please_forward, to = echo_server, a = 1, b = "xyz"))
 
-send(forwarder[:mailbox], (; msg = :please_forward, to = echo_server[:mailbox], a = 1, b = "xyz"))
-send(forwarder[:mailbox], (; msg = :please_forward, to = sink_server[:mailbox], a = 0, b = "it's all over!"))
+forwarder = spawn(forward)
+sink_server = spawn(sink)
+send(forwarder, (; msg = :please_forward, to = sink_server, a = 0, b = "it's all over!"))
+
+sink_server.mailbox.queue
 
 
 ##
 
-function ping(b::Mailbox, s)
-    send(s, (; msg = :ping, from = b, p = "this is a ping"))
-    m = rec(b, [_ -> true])
-    print(string((; received_q = m)) * "\n")
+function ping(self::Mailbox, pong)
+    send(pong, (; msg = :ping, from = self, p = "this is a ping"))
+    m = rec(self, [_ -> true])
+    print(string((; received = m)) * "\n")
 end
 
 echo_server = spawn(echo)
-pinger = spawn(b -> ping(b, echo_server[:mailbox]))
+pinger = spawn(b -> ping(b, echo_server)); # without `;` the output in the repl is messy!
 
-function pong(b::Mailbox) # , sink::Mailbox)
+
+function pong(self::Mailbox) 
     while true
-        m = rec(b, [msg(:ping)])
-        m = (; msg = :pong, q = m)
-        # send(sink, m)
-        send(m[:from], m)
+        m = rec(self, [msg(:ping)])
+        send(m[:from], (; msg = :pong, q = m))
     end
 end
 
+
+pong_server = spawn(pong)
+
 sink_server = spawn(sink)
+send(pong_server, (; msg = :ping, from = sink_server, p = "sink this"))
 
-pong_server = spawn(b -> pong(b, sink_server[:mailbox]))
+echo_server = spawn(echo)
+send(pong_server, (; msg = :x, from = echo_server, p = "echo this"))
 
-pinger = spawn(b -> ping(b, pong_server[:mailbox]))
+pinger = spawn(b -> ping(b, pong_server)); # see comment above
 
 
 m = (; msg = :ping, from = b, p = "whoaa")
